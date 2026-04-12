1be7152e15367c107791093c210958bbe9fc7b89
nthBit <207577365+nthBit@users.noreply.github.com>
2026-04-12 17:02:41 +0100

    Complete first rendition of rock-paper-scissors
    - Game plays correctly
    - Code needs simplifying
    - GUI needs to be added

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# rock-paper-scissors
+# Pseudocode
+
+<!-- computerChoice
+    Produce a random number between 0 and 2
+    Assign number to value (rock, paper, scissors)
+    Return value -->
+
+<!-- playerChoice
+    ask for input
+    Assign a number 0 to 2 based on choice
+    Assign number to value (rock, paper, scissors)
+    Return value  -->
+
+<!-- compareChoices
+    If computerChoice = 1 and playerChoice = 0:
+        winner = computer
+    Else if computerChoice = 2 and playerChoice = 1:
+        winner = computer
+    Else if computerChoice = 0 and playerChoice = 2:
+        winner = computer
+    Else if computerChoice === playerChoice:
+        winner = draw
+    Else:
+        winner = player
+    return winner -->
+
+printResult
+    print computerChoice vs playerChoice
+    print winner
--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-console.log("Hello world!")
+function playerChoice() {
+    let num = Number(prompt("Choose: Rock = 0; Paper = 1; Scissors = 2."));
+    return convertValue(num)
+}
+
+function computerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    return convertValue(num)
+}
+
+function convertValue(num) {
+    if (num === 0) {
+        return "rock";
+    } else if (num === 1) {
+        return "paper";
+    } else if (num === 2) {
+        return "scissors"
+    }
+}
+
+function compareChoices(computer, player) {
+    if (computer === "paper" && player === "rock") {
+        return "computer";
+    } else if (computer === "scissors" && player === "paper") {
+        return"computer";
+    } else if (computer === "rock" && player === "scissors") {
+        return "computer";
+    } else if (computer === player) {
+        return "draw";
+    } else {
+        return "player";
+    }
+}
+
+const user = playerChoice();
+const opponent = computerChoice();
+const gameOver = compareChoices(opponent, user);
+
+
+function finalMessage() {
+    console.log(`Player chooses: ${user}.`);
+    console.log(`Computer chooses: ${opponent}.`);
+
+    if (gameOver === "player") {
+        console.log("You win!");
+    } else if (gameOver === "computer") {
+        console.log("Computer wins.");
+    } else {
+        console.log("Draw.")
+    }
+}
+
+finalMessage();
